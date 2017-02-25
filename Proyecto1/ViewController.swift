e75b0b172ac5a35c6eecf8321cb7432940119a44
Hsing-Li <chang@hsing.li>
2017-02-24 19:27:39 -0600

    Initial Commit

---
+++ Proyecto1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto1
+//
+//  Created by Hsing-Li on 2/24/17.
+//  Copyright © 2017 Hsing-Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
